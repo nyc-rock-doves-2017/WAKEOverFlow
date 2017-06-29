a053fe6f64419b53acfff248fc1282464cf1bfa7
Eddie Harmon <eddieharmon@hotmail.com>
2017-06-29 18:20:11 -0400

    add votes controller with RESTful routes.

---
+++ app/controllers/votes.rb
@@ -0,0 +1,20 @@
+get '/votes' do
+  "display all votes"
+end
+
+get '/votes/new' do
+  "Form for new votes goes here"
+end
+
+post '/votes' do
+end
+
+get '/votes/:id' do
+  "Here's a particular vote"
+end
+
+put '/votes/:id' do
+end
+
+delete '/votes/:id' do
+end
