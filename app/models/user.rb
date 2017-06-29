58d326d194cd473df2eb332a6ecd77354e79d275
Kelsey Malone <kelsey.malone@gmail.com>
2017-06-29 12:10:29 -0400

    Create seed file

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   has_many :answers
   has_many :comments
   has_many :votes
+
+  def password=(string)
+    self.password_hash = string
+    # Replace with BCrypt Methods
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,47 @@
-# 20.times do
-#   post = Post.create!( title: Faker::Company.catch_phrase,
-#                username: Faker::Internet.user_name,
-#                comment_count: rand(1000),
-#                created_at: Time.now - rand(20000))
-#
-#   vote_count = rand(100)
-#   vote_count.times do
-#     post.votes.create!(value: 1)
-#   end
-# end
+10.times do
+  users = User.create!( username: Faker::Internet.user_name,
+                        email: Faker::Internet.email,
+                        password: 'password')
+end
+
+20.times do
+  questions = Question.create!( content: Faker::Lorem.sentence(1),
+                                user: User.all.sample)
+end
+
+40.times do
+  answers = Answer.create!( content: Faker::Lorem.sentence(2),
+                            user: User.all.sample,
+                            question: Question.all.sample)
+end
+
+80.times do
+  comment = Comment.new( content: Faker::Lorem.sentence(1),
+                          user: User.all.sample)
+
+  if rand(1..6) <= 3
+    comment.commentable = Question.all.sample
+  else
+    comment.commentable = Answer.all.sample
+  end
+
+  comment.save
+end
+
+comments = Comment.all
+
+200.times do
+  vote = Vote.new( score: (rand(1..2) == 1 ? (-1) : (1)),
+                    user: User.all.sample)
+
+  roll = rand(1..9)
+  if roll <= 3
+    vote.voteable = Question.all.sample
+  elsif roll > 3 && roll <= 6
+    vote.voteable = Answer.all.sample
+  else
+    vote.voteable = comments.sample
+  end
+
+  vote.save
+end
