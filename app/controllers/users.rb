42c893b24609bb477c342f771dbbc6e7d16d0f6f
Wan Tsui <wanytsui@gmail.com>
2017-06-29 18:30:09 -0400

    Update user signup and user login. Validation doesn't work for user so it's been commented out.

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -8,8 +8,9 @@ post '/users' do
   @new_user = User.new(params[:user_information])
   if @new_user.valid?
     create_user
-    redirect '/users/new'
+    redirect '/'
   else
+    @new_user_errors_messages = @new_user.errors.messages
     erb :'/users/new'
   end
 end
