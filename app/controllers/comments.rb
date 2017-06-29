a3ef20261e3cda8a70c2dd553e8032351fa32525
Eddie Harmon <eddieharmon@hotmail.com>
2017-06-29 15:34:02 -0400

    make view file for comments with text input field.

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -8,4 +8,31 @@ post '/comments/:id/vote/new' do
   else
     redirect "/questions/#{comment.commentable.question.id}"
   end
+
+get '/comments' do
+  "displays all comments"
+end
+
+get '/comments/new' do
+  # "Makes new comments"
+  erb :'/comments'
+end
+
+post '/comments' do
+end
+
+get '/comments/:id' do
+  "Displays a specific comment"
+end
+
+get '/comments/:id/edit' do
+  "Form for editing comments"
+end
+
+put '/comments/:id' do
+  "updates a comment"
+end
+
+delete '/comments/:id' do
+  "deletes a comments"
 end
