ee02664fabffe392f2c05827bd9d292457d04791
Wan Tsui <wanytsui@gmail.com>
2017-06-29 15:45:45 -0400

    Add basic user signup form

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,54 +4,17 @@ class User < ApplicationRecord
   has_many :comments
   has_many :votes
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  include BCrypt
+  validates :username, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+  validates :password_hash, presence: true
 
   def password
-    @password ||= Password.new(password_hash)
+    @password ||= BCrypt::Password.new(password_hash)
   end
-  def password=(new_password)
-    @password = Password.create(new_password)
+
+  def password=(password_string)
+    @password = BCrypt::Password.create(password_string)
     self.password_hash = @password
   end
+
 end
