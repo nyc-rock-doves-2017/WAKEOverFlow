4536b11f2db4aeca55ac7b47073ee255914d21bc
Eddie Harmon <eddieharmon@hotmail.com>
2017-06-29 19:21:24 -0400

    add instance variable inside comments route. add logic to comments index view file to display all comments.

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -11,6 +11,8 @@ post '/comments/:id/vote/new' do
 
 get '/comments' do
   "displays all comments"
+  @comments = Comment.all
+  erb :'/comments/index'
 end
 
 get '/comments/new' do
@@ -19,26 +21,30 @@ get '/comments/new' do
 end
 
 post '/comments' do
-  @comment = Comment.new(params[:comment])
-    if @comment.save
-    redirect '/comments'
-    else
-      puts "Please enter a legitimate comment"
-    end
+  # @comment = Comment.new(params[:comment])
+  #   if @comment.save
+  #   redirect '/comments'
+  #   else
+  #     erb :'/comments/new'
+  #   end
 end
 
 get '/comments/:id' do
   "Displays a specific comment"
+  erb :'/comments/show'
 end
 
 get '/comments/:id/edit' do
   "Form for editing comments"
+  erb :'/comments/edit'
 end
 
 put '/comments/:id' do
   "updates a comment"
+  erb :'/comments/edit'
 end
 
 delete '/comments/:id' do
   "deletes a comments"
+  redirect '/comments'
 end
